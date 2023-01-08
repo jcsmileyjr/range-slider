f64143758cc9f32a183eabf51b7b03293fd8d4aa
Smiley <jcsmileyjr@gmail.com>
2023-01-08 08:20:04 -0600

    Style changes with emphasis on the Dollar amount

--- react-range-slider/src/App.js
+++ react-range-slider/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
         <main>
-            <h1><span className="range__amountDollarSign--style">$</span> 16.00</h1>
+            <h1><span className="range__amountDollarSign--style">$</span>16.00</h1>
             <input className="range__inputRange--style" type="range" min="1" max="20" value="0" />
             <button className="range__buyButton--style" type="button">Buy Now</button>
         </main>
