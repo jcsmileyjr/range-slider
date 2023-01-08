fbe6e45bcc4860205c872dc42987987680b2679b
Smiley <jcsmileyjr@gmail.com>
2023-01-08 07:54:59 -0600

    Documentation: Initial TODO planning

--- react-range-slider/src/App.js
+++ react-range-slider/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+/**
+ * Simple React project featuring a range input displaying a dollar amount
+ * TODO - Setup the layout (background and center div)
+ * TODO - Setup basic HTML elements for the project
+ * TODO - Setup basic styling of the project
+ * TODO - Setup basic functionlaity of the project
+ * TODO - Fix issues
+ * TODO - Setup basic README of the project
+ */
 function App() {
   return (
     <div className="App">
-
+        Hello World
     </div>
   );
 }
