b0e737d2175ee27b19036c8f5ae5d02ec8136353
Smiley <jcsmileyjr@gmail.com>
2023-01-10 10:46:19 -0600

    Vanilla JS version: Documentation

--- vanilla-JS-range-slider/index.js
+++ vanilla-JS-range-slider/index.js
@@ -1,8 +1,10 @@
 const inputRange = document.querySelector('input[name="inputRange"]');
 const amount = document.querySelector('#amount');
 
+/***
+ * Function to update the dollar amount and input range when user moves the range slider.d
+ */
 inputRange.addEventListener('change', function (event) {
-    console.log(event.target.value);
-    inputRange.style.backgroundSize = `${(event.target.value * 100) / 20}% 100%`;
     amount.innerHTML= event.target.value;
+    inputRange.style.backgroundSize = `${(event.target.value * 100) / 20}% 100%`;
 });
